85f4d22384b0379ac565dbfa8d10e53311fb9c1a
a-powers <awpowers@gmail.com>
2021-02-15 01:34:34 +0000

    update

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
-# 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
-
-
-#Write your code below this row 👇
 
 
 total_heights = 0
 for height in student_heights:
     total_heights += height
-
 sum_of_heights = total_heights
 
 total_students = 0
 for student in student_heights:
     total_students += 1
+count_of_students = total_students
 
-sum_of_students = total_students
+average_height = sum_of_heights//count_of_students
 
-print((sum_of_heights//sum_of_students))
+print(f"The average student height is {average_height}.")
